9f0ea52acb67abd60f44376e8f26034e24a0cdbe
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2025-05-10 13:34:35 +0200

    cleanup

--- inst/rexams/tratio.pl
+++ inst/rexams/tratio.pl
@@ -2,5 +2,5 @@
 expert(tratio(X, Mu, S, N), frac(X - Mu, S / sqrt(N)), "expert").
 
 % Mistakes
-buggy(frac(X - Mu, S / SQRTN), X - frac(Mu, S) / SQRTN, "buggy(paren)").
+buggy(frac(X - Mu, S / SQRTN), X - frac(Mu, S) / SQRTN, buggy(paren)).
 buggy(sqrt(N), N, "buggy(sqrt)").
