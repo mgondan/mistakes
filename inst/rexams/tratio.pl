9e185ce12f61093dac31b5d773d1adcacff73193
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2025-05-18 21:37:24 +0200

    buggy rule mu for tratio

--- inst/rexams/tratio.pl
+++ inst/rexams/tratio.pl
@@ -8,10 +8,15 @@ buggy(dfrac(X - Mu, S / SQRTN), X - dfrac(Mu, S) / SQRTN, paren).
 
 buggy(sqrt(N), error(instead(N, sqrt(N))), sqrt(N)).
 
+buggy(tratio(X, Mu, S, N), 
+    dfrac(error(omit_right(X - Mu)), S / sqrt(N)), mu(Mu)).
+
 % Feedback
-msg(tratio, "Correctly identified the expression for the $t$-ratio.").
+msg(tratio, "Correctly identified the expression for the ~m-ratio."-[t]).
 
 msg(paren, "Please do not forget the parentheses around the numerator and 
     the denominator of a fraction.").
 
-msg(sqrt(X), "Please do not omit the square root around $~w$."-[X]).
+msg(sqrt(X), "Please do not omit the square root around ~m."-[X]).
+
+msg(mu(Mu), "Do not omit the null hypothesis ~m in the ~m-ratio."-[Mu, t]).
