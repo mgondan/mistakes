bac09d2c4780c5d3918ea2971dbb1fb7be46616f
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2025-06-13 23:15:37 +0200

    buggy rule for wrong tail

--- inst/rexams/pvalue.pl
+++ inst/rexams/pvalue.pl
@@ -10,11 +10,20 @@ intermediate(tpaired/4).
 expert(tpaired(X, Mu, S, N), dfrac(X - Mu, S / sqrt(N)), tratio, []).
 
 intermediate(twotailed/2).
-expert(twotailed(T, DF), 2 * pt(dist('T', T, "upper"), DF, tail("upper")), pvalue, []).
+expert(X, Y, twotailed, []) :-
+    X = twotailed(T, DF), 
+    Y = pt(dist('T', T, "two.sided"), DF, tail("two.sided")). 
+
+buggy(X, Y, lower, []) :-
+    X = twotailed(T, DF),
+    Y = pt(instead(dist('T', T, "lower"), dist('T', T, "two.sided")), DF,
+          instead(tail("lower"), tail("two.sided"))).
 
 msg(paired, 
   "Correctly recognised the problem as a ~m-test for paired samples."-[t]).
 
 msg(tratio, "Correctly identified the expression for the ~m-ratio."-[t]).
 
-msg(pvalue, "Correctly determined the two-tailed ~m-value."-[p]).
+msg(twotailed, "Correctly determined the two-tailed ~m-value."-[p]).
+
+msg(lower, "The result matches the lower one-tailed ~m-value."-[p]).
