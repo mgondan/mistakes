68b717f865850e909185ce4dfc88b8e5c46b024b
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2025-06-14 21:13:34 +0200

    buggy rules for both tails

--- inst/rexams/pvalue.pl
+++ inst/rexams/pvalue.pl
@@ -12,12 +12,17 @@ expert(tpaired(X, Mu, S, N), dfrac(X - Mu, S / sqrt(N)), tratio, []).
 intermediate(twotailed/2).
 expert(X, Y, twotailed, []) :-
     X = twotailed(T, DF), 
-    Y = pt(dist('T', T, "two.sided"), DF, tail("two.sided")). 
+    Y = pt1(dist('T', T, "two.sided"), DF, tail("two.sided")). 
 
 buggy(X, Y, lower, []) :-
     X = twotailed(T, DF),
-    Y = pt(instead(dist('T', T, "lower"), dist('T', T, "two.sided")), DF,
+    Y = pt1(instead(dist('T', T, "lower"), dist('T', T, "two.sided")), DF,
           instead(tail("lower"), tail("two.sided"))).
+
+buggy(X, Y, upper, []) :-
+    X = twotailed(T, DF),
+    Y = pt1(instead(dist('T', T, "upper"), dist('T', T, "two.sided")), DF,
+          instead(tail("upper"), tail("two.sided"))).
 
 msg(paired, 
   "Correctly recognised the problem as a ~m-test for paired samples."-[t]).
@@ -27,3 +32,5 @@ msg(tratio, "Correctly identified the expression for the ~m-ratio."-[t]).
 msg(twotailed, "Correctly determined the two-tailed ~m-value."-[p]).
 
 msg(lower, "The result matches the lower one-tailed ~m-value."-[p]).
+
+msg(upper, "The result matches the upper one-tailed ~m-value."-[p]).
