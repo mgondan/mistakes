f658206cfd658c24c136a409a5c638266684f7d7
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2025-06-16 10:52:47 +0200

    man pages

--- R/mistakes.R
+++ R/mistakes.R
@@ -11,7 +11,7 @@
   mathml::hook(error(.X), .X)
 }
 
-expert1 <- function(expr)
+.expert1 <- function(expr)
 {
   if(!is.call(expr))
     stop("expert: expression is not a call")
@@ -19,9 +19,16 @@ expert1 <- function(expr)
   expr[[1]] == "expert"
 }
 
-expert <- Vectorize(expert1)
+#' Check if a list includes only expert rules
+#' (for internal use)
+#'
+#' @md
+#' 
+#' @param expr
+#' list of R calls of the form expert/3 or buggy/3
+expert <- Vectorize(.expert1)
 
-feedback1 <- function(expr)
+.feedback1 <- function(expr)
 {
   if(!is.call(expr))
     stop("feedback: expression is not a call")
@@ -41,24 +48,78 @@ feedback1 <- function(expr)
   stop("feedback: expression should be expert/1 or buggy/1")
 }
 
+#' Extract feedback from a list
+#' (for internal use)
+#'
+#' @md
+#' 
+#' @param expr
+#' list of R calls of the form expert/3 or buggy/3
+#'
 feedback <- function(expr)
-  Vectorize(feedback1)(hooked(expr))
+  Vectorize(.feedback1)(hooked(expr))
 
+#' Evaluate argument of error/1
+#' (for internal use)
+#'
+#' @md
+#' 
+#' @param expr
+#' R call of the form error/1
+#'
 error <- function(expr)
   return(expr)
 
+#' Evaluate argument 2 of omit_right/3
+#' (for internal use)
+#'
+#' @md
+#' 
+#' @param expr
+#' R call of the form omit_right/3
+#'
 omit_right <- function(expr)
   substitute(expr)[[3]]
 
+#' Evaluate argument of {}/1
+#' (for internal use)
+#'
+#' @md
+#' 
+#' @param expr
+#' R call
+#'
 `{}` <- function(expr)
   return(expr)
 
+#' Evaluate arguments of ;/2
+#' (for internal use)
+#'
+#' @md
+#' 
+#' @param expr1
+#' @param expr2
+#' R calls
+#'
 `;` <- function(expr1, expr2)
 {
   expr1
   return(expr2)
 }
 
+#' Pretty print pvalue/1
+#'
+#' @md
+#' 
+#' @param x
+#' number 0 < x < 1
+#'
+#' @examples
+#' pvalue(0)
+#' pvalue(0.002)
+#' pvalue(0.062)
+#' pvalue(0.212)
+#' pvalue(1)
 pvalue <- function(x)
 {
   if(x > 0.995)
