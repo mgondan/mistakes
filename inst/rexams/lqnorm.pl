a144b2d4d98fd3f023384b4ccbb9dfcddd39c660
Alfarone <c6031213@uibk.ac.at>
2026-09-23 11:46:37 +0200

    new feedbacks

--- inst/rexams/lqnorm.pl
+++ inst/rexams/lqnorm.pl
@@ -6,6 +6,7 @@
 intermediate(lqnorm/3).
 intermediate(zquant/1).
 intermediate(invz/3).
+
 expert(X, Y, lqnorm, []) :-
     X = lqnorm(P, Mu, Sigma2),
     Y = { '<-'(z, zquant(P))
@@ -20,24 +21,23 @@ buggy(X, Y, sqrt(Sigma^2), []) :-
     X = invz(Z, Mu, Sigma^2),
     Y = Mu + Z * error(instead(Sigma^2, Sigma)).
 
-expert(X, Y, lower, []) :-
+expert(X, Y, lower(P), []) :-
     X = zquant(P), 
     Y = qnorm1(quant('Z', P, "lower"), tail("lower")). 
 
-buggy(X, Y, upper, []) :-
+buggy(X, Y, upper(P), []) :-
     X = zquant(P), 
     Y = qnorm1(quant('Z', P, "upper"), tail("upper")). 
 
 % Feedback
-msg(lqnorm, "The quantile function of the Normal distribution must be used.").
+msg(lqnorm, "This is a Normal distribution quantile problem.").
 
-msg(zfrac, "The inverse ~m-transformation is applied."-[z]).
+msg(zfrac, "You correctly transformed the ~m-value back to the original scale."-[z]).
 
-msg(zquant, "The area is given by the difference of the distribution function
-    at ~m and ~m."-[subscript(z, 2), subscript(z, 1)]).
+msg(zquant, "The area is given by the difference of the distribution function at ~m and ~m."-[subscript(z, 2), subscript(z, 1)]).
 
-msg(lower, "The result is given by the quantile function of the lower tail.").
+msg(lower(P), "You correctly used the lower tail of the Normal distribution. The exercise asks for the value of X that is larger than the proportion ~m of observations. This means that the same proportion of the observations lies below X, so the required quantile is obtained using the lower tail." -[P]).
 
-msg(upper, "The upper tail was used (instead of the lower tail).").
+msg(upper(P), "You used the wrong tail of the Normal distribution. The exercise asks for the value of X that is larger than the proportion ~m of observations. This means that the same proportion of the observations lies below X. Therefore, the lower tail must be used. To solve this exercise you used the upper tail instead." -[P]).
 
-msg(sqrt(Sigma2), "Please do not omit the square root around ~m."-[Sigma2]).
+msg(sqrt(Sigma2), "Remember to take the square root of the variance ~m before using it in the inverse ~m-transformation."-[Sigma2, z]).
